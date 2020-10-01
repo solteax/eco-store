01c8933db5f2aae227a25ecdcfe50d9beeea6483
itPointOfLife <it_original@ukr.net>
2020-10-01 11:35:54 +0300

    add screen sizes to tilwindcss containers

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,15 @@ module.exports = {
         'custom-green-hover': '#97cb49',
       },
     },
+
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'xxl': '1440px',
+      'xxxl': '1824px',
+    }
   },
   variants: {},
   plugins: [],
